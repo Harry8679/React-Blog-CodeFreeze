0ea528c216d39b0f2f72d345d9bc27bb080fe644
Harry EMARH <emarh.harry.code@gmail.com>
2024-06-28 23:56:47 +0200

    Header file added

---
+++ src/components/Home/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='home-header'>
+        <h2>Inc. This morning</h2>
+        <h1>
+            <span>"</span> Blog <span>"</span>
+        </h1>
+    </div>
+  )
+}
+
+export default Header

4d5d55f31a4f70a5d863a882164abb511ddb9f48
Harry EMARH <emarh.harry.code@gmail.com>
2024-06-28 17:03:24 +0200

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World !</h1>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

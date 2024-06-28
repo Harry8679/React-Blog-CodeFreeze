3e257da792319c67aaa57ab0fbca021935455d3d
Harry EMARH <emarh.harry.code@gmail.com>
2024-06-28 23:58:14 +0200

    Paragraph updated

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='home-header'>
-        <h2>Inc. This morning</h2>
-        <h1>
-            <span>"</span> Blog <span>"</span>
-        </h1>
-        <p>
-            Awesome place to make oneself
-        </p>
+    <div className="home-header">
+      <h2>Inc. This morning</h2>
+      <h1>
+        <span>"</span> Blog <span>"</span>
+      </h1>
+      <p>
+        Awesome place to make oneself <br /> productive and entertained through
+        daily updates.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
